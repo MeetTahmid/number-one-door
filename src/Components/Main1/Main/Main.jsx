c85b5ad7010e1d028e5f4dbc38e985104471cc7b
MeetTahmid <redfoxtahmid@gmail.com>
2024-04-23 12:02:23 +0600

    added responsive in full website 1

--- src/Components/Main1/Main/Main.jsx
+++ src/Components/Main1/Main/Main.jsx
@@ -6,11 +6,11 @@ import React from 'react';
 const Main = ({ main }) => {
     const { name, image } = main;
     return (
-        <div className='mt-8 mb-12 w-full hover:translate-y-4 transition-all duration-700'>
-            <img className='w-[800px]' src={image} alt="" />
+        <div className='mt-8 mb-12 rounded-lg shadow-xl w-full hover:translate-y-4 rounded-lghover:translate-y-4 transition-all duration-700'>
+            <img className='rounded-lg' src={image} alt="" />
             <div className='py-6 px-5'>
-                <h3 className='text-2xl font-bold mb-3'>কাঠের দরজা</h3>
-                <p>এই দরজা টি কিনতে স্ক্রীনশট নিয়ে Whatsapp এবং Facebook Page এ যোগাযোগ করুন</p>
+                <h3 className='text-xl font-bold mb-3'>{name}</h3>
+                <a href="#" className='btn btn-ghost bg-base-200'>Buy Now</a>
             </div>
         </div>
     );
--- src/Components/Main1/Mains/Mains.jsx
+++ src/Components/Main1/Mains/Mains.jsx
@@ -13,7 +13,7 @@ const Mains = () => {
     return (
         <div className="px-7 lg:px-36">
             <h1 className="mt-16 text-2xl font-bold "><span className="">কাঠের দরজা</span></h1>
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-10">
+            <div className="grid grid-cols-2 lg:grid-cols-7 gap-10">
 
                 {
                     mains.map(main => <Main key={main.id} main={main}></Main>)
